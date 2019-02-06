6a5cff10563e3d6a1ceffd670733693579faf2b5
sd1998 <shashvat51@gmail.com>
2019-02-06 16:02:04 +0530

    counter added

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,102 +1,112 @@
-public class MergeSort {
+import java.util.Random;
+
+class MergeSort {
+    static int counter = 0;
+
     // Merges two subarrays of arr[].
-    // First subarray is arr[l..m]
-    // Second subarray is arr[m+1..r]
-    void merge(int arr[], int l, int m, int r)
-    {
-        // Find sizes of two subarrays to be merged
+// First subarray is arr[l..m]
+// Second subarray is arr[m+1..r]
+    void merge(int arr[], int l, int m, int r) {
+// Find sizes of two subarrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
 
-        /* Create temp arrays */
-        int L[] = new int [n1];
-        int R[] = new int [n2];
+/* Create temp arrays */
+        int L[] = new int[n1];
+        int R[] = new int[n2];
 
-        /*Copy data to temp arrays*/
-        for (int i=0; i<n1; ++i)
+/*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
             L[i] = arr[l + i];
-        for (int j=0; j<n2; ++j)
-            R[j] = arr[m + 1+ j];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
 
 
-        /* Merge the temp arrays */
+/* Merge the temp arrays */
 
-        // Initial indexes of first and second subarrays
+// Initial indexes of first and second subarrays
         int i = 0, j = 0;
 
-        // Initial index of merged subarry array
+// Initial index of merged subarry array
         int k = l;
-        while (i < n1 && j < n2)
-        {
-            if (L[i] <= R[j])
-            {
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
                 arr[k] = L[i];
                 i++;
-            }
-            else
-            {
+            } else {
                 arr[k] = R[j];
                 j++;
             }
             k++;
+            counter++;
+
         }
 
-        /* Copy remaining elements of L[] if any */
-        while (i < n1)
-        {
+/* Copy remaining elements of L[] if any */
+        while (i < n1) {
             arr[k] = L[i];
             i++;
             k++;
+            counter++;
+
         }
 
-        /* Copy remaining elements of R[] if any */
-        while (j < n2)
-        {
+/* Copy remaining elements of R[] if any */
+        while (j < n2) {
             arr[k] = R[j];
             j++;
             k++;
+            counter++;
+
         }
+
     }
 
     // Main function that sorts arr[l..r] using
-    // merge()
-    void sort(int arr[], int l, int r)
-    {
-        if (l < r)
-        {
-            // Find the middle point
-            int m = (l+r)/2;
+// merge()
+    void sort(int arr[], int l, int r) {
+        if (l < r) {
+// Find the middle point
+            int m = (l + r) / 2;
 
-            // Sort first and second halves
+// Sort first and second halves
             sort(arr, l, m);
-            sort(arr , m+1, r);
+            sort(arr, m + 1, r);
 
-            // Merge the sorted halves
+// Merge the sorted halves
             merge(arr, l, m, r);
         }
+
     }
 
     /* A utility function to print array of size n */
-    static void printArray(int arr[])
-    {
+    static void printArray(int arr[]) {
         int n = arr.length;
-        for (int i=0; i<n; ++i)
+        for (int i = 0; i < n; ++i)
             System.out.print(arr[i] + " ");
         System.out.println();
     }
 
     // Driver method
-    public static void main(String args[])
-    {
-        int arr[] = {12, 11, 13, 5, 6, 7};
+    public static void main(String args[]) {
+        Random rand = new Random();
+        int arr[] = new int[8];
+        for (int i = 0; i < 8; i++) {
+            arr[i] = rand.nextInt(1000);
+        }
 
         System.out.println("Given Array");
         printArray(arr);
 
         MergeSort ob = new MergeSort();
-        ob.sort(arr, 0, arr.length-1);
+        long start = System.nanoTime();
+        ob.sort(arr, 0, arr.length - 1);
+        long end = System.nanoTime();
+        System.out.println(end - start);
 
         System.out.println("\nSorted array");
         printArray(arr);
-    } 
+        System.out.println(counter);
+    }
 }
+/* This code is contributed by Rajat Mishra */
