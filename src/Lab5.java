6f0aa1ba27f057e25608065a487c63704c383c05
sd1998 <shashvat51@gmail.com>
2019-02-20 15:56:08 +0530

    changed to recursive

--- src/Lab5.java
+++ src/Lab5.java
@@ -6,48 +6,41 @@ public class Lab5 {
 
     public static BufferedReader reader;
 
-    static void generateSquare(int n)
-    {
-        int[][] magicSquare = new int[n][n];
-        int i = n/2;
-        int j = n-1;
-        for (int num=1; num <= n*n; )
-        {
-            if (i==-1 && j==n)
-            {
-                j = n-2;
-                i = 0;
-            }
-            else
-            {
-                if (j == n)
-                    j = 0;
-                if (i < 0)
-                    i=n-1;
-            }
-            if (magicSquare[i][j] != 0)
-            {
-                j -= 2;
-                i++;
-                continue;
-            }
-            else
-                magicSquare[i][j] = num++;
-            j++;  i--;
+    static int num, i, j;
+    static int magicSquare[][];
+
+    static void generateSquare(int n, int num) {
+        if (num == 0) {
+            return;
         }
-        System.out.println("The Magic Square for "+n+":");
-        System.out.println("Sum of each row or column "+n*(n*n+1)/2+":");
-        for(i=0; i<n; i++)
-        {
-            for(j=0; j<n; j++)
-                System.out.print(magicSquare[i][j]+" ");
-            System.out.println();
+        System.out.println(i + " " + j);
+        if (i == -1 && j == n) {
+            j = n - 2;
+            i = 0;
         }
+        else {
+            if (j == n)
+                j = 0;
+            if (i < 0)
+                i = n - 1;
+        }
+        if (magicSquare[i][j] != 0) {
+            j -= 2;
+            i++;
+            generateSquare(n, num - 1);
+        } else
+            magicSquare[i][j] = num++;
+        j++;
+        i--;
+        generateSquare(n, num - 1);
     }
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
-        generateSquare(n);
+        i = n / 2;
+        j = n - 1;
+        magicSquare = new int[n][n];
+        generateSquare(n, n * n);
     }
 }
